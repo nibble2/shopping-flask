f451031641c9ffd028e664e5e811ec078c04e369
nibble2 <nibblekk@gmail.com>
2019-10-18 19:57:01 +0900

    주소 받아오기 예제 변경

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ function myOrder() {
     let store = 'nibble'
     let name = $('#order_name').val();
     let phone = $('#order_phone').val();
-    let address = $('#sample3_address').val()+ " " + $('#sample3_detailAddress').val()+" "+ $('#sample3_extraAddress').val();
+    let address = $('#sample6_address').val()+ " " + $('#sample6_detailAddress').val();
     let count = $('#order_count').val();
     let item = $('#order_count').val();
 
@@ -61,20 +61,10 @@ function make_order(item, name, phone, count, address) {
 }
 
 // 다음 주소 api
-// 우편번호 찾기 찾기 화면을 넣을 element
-var element_wrap = document.getElementById('wrap');
-
-function foldDaumPostcode() {
-    // iframe을 넣은 element를 안보이게 한다.
-    element_wrap.style.display = 'none';
-}
-
-function sample3_execDaumPostcode() {
-    // 현재 scroll 위치를 저장해놓는다.
-    var currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+function sample6_execDaumPostcode() {
     new daum.Postcode({
         oncomplete: function(data) {
-            // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
             // 각 주소의 노출 규칙에 따라 주소를 조합한다.
             // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
@@ -104,35 +94,19 @@ function sample3_execDaumPostcode() {
                     extraAddr = ' (' + extraAddr + ')';
                 }
                 // 조합된 참고항목을 해당 필드에 넣는다.
-                document.getElementById("sample3_extraAddress").value = extraAddr;
+                document.getElementById("sample6_extraAddress").value = extraAddr;
 
             } else {
-                document.getElementById("sample3_extraAddress").value = '';
+                document.getElementById("sample6_extraAddress").value = '';
             }
 
             // 우편번호와 주소 정보를 해당 필드에 넣는다.
-            document.getElementById('sample3_postcode').value = data.zonecode;
-            document.getElementById("sample3_address").value = addr;
+            document.getElementById('sample6_postcode').value = data.zonecode;
+            document.getElementById("sample6_address").value = addr;
             // 커서를 상세주소 필드로 이동한다.
-            document.getElementById("sample3_detailAddress").focus();
-
-            // iframe을 넣은 element를 안보이게 한다.
-            // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
-            element_wrap.style.display = 'none';
-
-            // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
-            document.body.scrollTop = currentScroll;
-        },
-        // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
-        onresize : function(size) {
-            element_wrap.style.height = size.height+'px';
-        },
-        width : '100%',
-        height : '100%'
-    }).embed(element_wrap);
-
-    // iframe을 넣은 element를 보이게 한다.
-    element_wrap.style.display = 'block';
+            document.getElementById("sample6_detailAddress").focus();
+        }
+    }).open();
 }
 
 // 이름 정규식
